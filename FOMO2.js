ee1fee0bf016306d1b0b83bac0ddd28b1e4e6813
Nicomontviv <81643452+Nicomontviv@users.noreply.github.com>
2022-11-30 20:26:30 -0300

    Add files via upload

---
+++ FOMO2.js
@@ -0,0 +1,19 @@
+
+let visitantes = 372;
+const iterarFomo2 = setInterval(mostrarFomo2, 30000);
+
+function mostrarFomo2(){
+    
+    fomo.style.transitionDelay = "1000ms";
+    fomoH6.innerHTML = ` <font color = rgb(94, 255, 0)>${visitantes}</font> personas estan viendo esta página`;
+    fomo.style.backgroundColor = `rgba(63, 80, 110, 0.856)`;
+    fomo.style.display = "block";
+    
+    setTimeout(dejarDeMostrar2, 5000);
+}
+
+function dejarDeMostrar2(){
+    fomo.style.transitionDelay = "500ms";
+    fomo.style.display = "none";
+    visitantes = visitantes + 6;
+}
